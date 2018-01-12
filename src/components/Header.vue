56bd890a912cd2013284e809803f34600f67fabe
Marcin Radziewicz <marcin.radziewicz@contractors.roche.com>
2018-01-12 15:03:18 +0100

    Toggable navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-    <b-navbar toggleable type="light" variant="light">
+    <b-navbar toggleable="md" type="light" variant="light" fixed>
       <router-link tag="b-navbar-brand" class="pointer" to="/" id="home">Security Panel</router-link>
-      <b-navbar-nav class="ml-auto">
-        <router-link tag="div" to="/phone" size="md">
-          <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>Phone</b-button>
-        </router-link>
-        <router-link tag="div" to="/gpio" size="md">
-          <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>GPIO</b-button>
-        </router-link>
-        <router-link tag="div" to="/gateway" size="md">
-          <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>SMS</b-button>
-        </router-link>
-        <router-link tag="div" to="/logs" size="md">
-          <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>Logs</b-button>
-        </router-link>
-      </b-navbar-nav>
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav class="ml-auto">
+          <router-link tag="div" to="/phone" size="md">
+            <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>Phone</b-button>
+          </router-link>
+          <router-link tag="div" to="/gpio" size="md">
+            <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>GPIO</b-button>
+          </router-link>
+          <router-link tag="div" to="/gateway" size="md">
+            <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>SMS</b-button>
+          </router-link>
+          <router-link tag="div" to="/logs" size="md">
+            <b-button variant="outline-primary" class="my-2 my-sm-0" right exact>Logs</b-button>
+          </router-link>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
