968684c84dca0ffc5c671ac4f97f3d6dad23dfea
Marcin Radziewicz <marcin.radziewicz@contractors.roche.com>
2018-01-16 09:50:22 +0100

    Updated component list

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Phone from '@/components/Phone'
 import Gateway from '@/components/Gateway'
 import Logs from '@/components/Logs'
 import Gpio from '@/components/Gpio'
+import Header from '@/components/Header'
+import Inbox from '@/components/Inbox'
+import Sent from '@/components/Sent'
 
-export { Home, Phone, Gateway, Logs, Gpio }
+export { Home, Phone, Gateway, Logs, Gpio, Header, Inbox, Sent }
